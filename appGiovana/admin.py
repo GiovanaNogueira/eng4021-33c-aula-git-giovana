aa615a72fd8d19230c626c918a51be25ab9b7a79
GiovanaNogueira <129424717+GiovanaNogueira@users.noreply.github.com>
2023-05-12 10:47:13 -0300

    Update admin.py

--- appGiovana/admin.py
+++ appGiovana/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin \\importa o módulo "admin" do Django, que é responsável por fornecer uma interface administrativa para gerenciamento de dados
-from .models import Dogs \\importa a classe "Dogs" criada em models 
-from .models import Metas \\importa a classe "Metas" criada em models 
+from django.contrib import admin #importa o módulo "admin" do Django, que é responsável por fornecer uma interface administrativa para gerenciamento de dados
+from .models import Dogs #importa a classe "Dogs" criada em models 
+from .models import Metas #importa a classe "Metas" criada em models 
 
-admin.site.register(Dogs) \\registra no página de acesso do administrador a classe "Dogs", para que seja possível editar a classe por lá
-admin.site.register(Metas) \\registra no página de acesso do administrador a classe "Metas", para que seja possível editar a classe por lá
+admin.site.register(Dogs) #registra no página de acesso do administrador a classe "Dogs", para que seja possível editar a classe por lá
+admin.site.register(Metas) #registra no página de acesso do administrador a classe "Metas", para que seja possível editar a classe por lá
